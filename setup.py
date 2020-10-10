d7ae51782d0a1aef0b9ac3c132acdb5818ca6d4d
UHl0aG9uZWVy <ksdgdfjhgaskfgdjsgf@mail.ru>
2020-10-10 12:14:44 +0800

    1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="sabrute",
-    version="1.0.4",
+    version="1.0.5",
     authors="rmdlv",
     author_email="ksdgdfjhgaskfgdjsgf@mail.ru",
     description="Программа для подбора паролей SAMP Mobile",
